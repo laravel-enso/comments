7e2f528f7608b6074ce2dd34cc1bf156db1dd786
aocneanu <aocneanu@gmail.com>
2017-03-20 14:01:35 +0000

    Apply fixes from StyleCI

--- src/Comment.php
+++ src/Comment.php
@@ -6,20 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 use LaravelEnso\TrackWho\Traits\UpdatedBy;
 
 /**
- * App\Comment
+ * App\Comment.
  *
- * @property integer $id
- * @property integer $user_id
- * @property integer $commentable_id
+ * @property int $id
+ * @property int $user_id
+ * @property int $commentable_id
  * @property string $commentable_type
  * @property string $body
- * @property boolean $is_edited
- * @property integer $updated_by
+ * @property bool $is_edited
+ * @property int $updated_by
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * @property-read \App\User $user
  * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $commentable
  * @property-read \App\User $updatedBy
+ *
  * @method static \Illuminate\Database\Query\Builder|\App\Comment whereId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Comment whereUserId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Comment whereCommentableId($value)
@@ -30,11 +31,11 @@ use LaravelEnso\TrackWho\Traits\UpdatedBy;
  * @method static \Illuminate\Database\Query\Builder|\App\Comment whereCreatedAt($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Comment whereUpdatedAt($value)
  * @mixin \Eloquent
+ *
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\User[] $tagged_users
  */
 class Comment extends Model
 {
-
     use UpdatedBy;
 
     public function user()
