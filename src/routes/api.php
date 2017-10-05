367105416e745d3daf2b1d4048c1ef1d207dd56f
mishu <mishu@xtelecom.ro>
2017-10-05 14:53:23 +0300

    refactor routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,14 +1,11 @@
 <?php
 
-Route::middleware(['auth:api', 'api', 'core'])
-    ->prefix('api')
+Route::middleware(['web', 'auth', 'core'])
+    ->prefix('api/core')->as('core.')
     ->namespace('LaravelEnso\CommentsManager\app\Http\Controllers')
     ->group(function () {
-        Route::prefix('core')->as('core.')
-            ->group(function () {
-                Route::get('comments/getTaggableUsers/{query?}', 'TaggableUserController')
-                    ->name('comments.getTaggableUsers');
+        Route::get('comments/getTaggableUsers/{query?}', 'TaggableUserController')
+            ->name('comments.getTaggableUsers');
 
-                Route::resource('comments', 'CommentController', ['except' => ['show', 'edit', 'create']]);
-            });
+        Route::resource('comments', 'CommentController', ['except' => ['show', 'edit', 'create']]);
     });
