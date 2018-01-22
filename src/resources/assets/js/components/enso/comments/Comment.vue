fa1a0ba5d0b9955ac1af46d533e7e15964bd4eac
mishu <mishu@xtelecom.ro>
2018-01-22 18:50:52 +0200

    implements the new card

--- src/resources/assets/js/components/enso/comments/Comment.vue
+++ src/resources/assets/js/components/enso/comments/Comment.vue
@@ -1,172 +1,237 @@
 <template>
 
-    <article class="media"
-        @mouseover="controls = true"
-        @mouseleave="!dialog ? controls = false: null">
-        <figure class="media-left">
-            <p class="image is-48x48">
-                <img :src="avatar"
-                    alt="User Avatar"
-                    class="avatar">
-            </p>
-        </figure>
-        <div class="media-content">
-            <div class="has-margin-bottom-medium has-text-grey" v-if="!isNew">
-                <a><b>{{ comment.owner.fullName }}</b></a>
-                <span>
-                    {{ comment.updated_at || comment.created_at | timeFromNow }}
-                </span>
-                <span v-if="comment.isEdited">
-                    &bull; {{ __('edited') }}
-                </span>
-                <div class="is-pulled-right is-flex"
-                    v-if="!isNew && !isEditing && controls">
-                    <button class="button is-naked is-small has-margin-right-small"
-                        v-if="comment.isEditable"
-                        @click="originalBody=comment.body;">
-                        <span class="icon is-small has-text-grey">
-                            <i class="fas fa-pencil-alt"></i>
-                        </span>
-                    </button>
-                    <popover placement="bottom-end"
-                        v-if="comment.isDeletable"
-                        @confirm="$emit('delete')"
-                        @show="dialog = true"
-                        @hide="dialog = controls = false">
-                        <button class="button is-naked is-small"
-                            @click="dialog=true">
-                            <span class="icon is-small has-text-grey">
-                                <i class="fas fa-trash-alt"></i>
-                            </span>
-                        </button>
-                    </popover>
-                </div>
-            </div>
-            <div v-html="highlightTaggedUsers"
-                class="comment-body"
-                v-if="!isEditing && !isNew">
-            </div>
-            <div v-else>
-                <inputor v-on="$listeners"
-                    :comment="comment">
-                </inputor>
-                <div>
-                    <button type="button" class="button is-small is-outlined has-margin-right-small"
-                        @click="isNew ? $emit('cancel-add') : cancelAdd()">
-                        {{ __('Cancel') }}
-                    </button>
-                    <button type="button" class="button is-small is-outlined is-success"
-                        @click="isNew ? $emit('save-comment') : update()">
-                        <span v-if="isNew">
-                            {{ __('Post') }}
-                        </span>
-                        <span v-else>
-                            {{ __('Update') }}
-                        </span>
-                    </button>
-                </div>
+    <card icon="fas fa-comments"
+        refresh
+        :search="comments.length > 1"
+        :title="title || __('Comments')"
+        :overlay="loading"
+        @refresh="refresh()"
+        :collapsed="!open || isEmpty"
+        ref="card"
+        @query-update="query = $event"
+        @expand="isEmpty && !comment ? $refs.card.collapse() : null"
+        :badge="count"
+        :controls="1">
+        <card-control slot="control-1"
+            @click="create()">
+            <span class="icon is-small">
+                <i class="fas fa-plus-square"></i>
+            </span>
+        </card-control>
+        <div class="has-padding-medium wrapper">
+            <comment is-new
+                :id="id"
+                :type="type"
+                v-if="comment"
+                :comment="comment"
+                :index="-1"
+                @cancel-add="comment = null"
+                @save-comment="add()">
+            </comment>
+            <comment v-for="(comment, index) in filteredComments"
+                :comment="comment"
+                :index="index"
+                @save-comment="update(comment)"
+                @delete="destroy(index)"
+                :key="index">
+            </comment>
+            <div class="has-text-centered has-margin-top-large">
+                <button class="button is-naked has-text-grey"
+                    @click="get()">
+                    &bull; &bull; &bull;
+                </button>
             </div>
         </div>
-    </article>
+    </card>
 
 </template>
 
 <script>
 
-import { mapGetters } from 'vuex';
-import Inputor from './Inputor.vue';
-import Popover from '../bulma/Popover.vue';
+import { mapGetters, mapState } from 'vuex';
+import Card from '../bulma/Card.vue';
+import CardControl from '../bulma/CardControl.vue';
+import Comment from './Comment.vue';
+
 
 export default {
-    name: 'Comment',
+    name: 'Comments',
 
-    components: { Inputor, Popover },
+    components: { Card, CardControl, Comment },
 
     props: {
-        comment: {
-            type: Object,
-        },
-        index: {
-            type: Number,
-            default: null,
-        },
-        isNew: {
+        open: {
             type: Boolean,
             default: false,
+        },
+        id: {
+            type: Number,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+        paginate: {
+            type: Number,
+            default: 5,
         },
     },
 
     computed: {
         ...mapGetters('locale', ['__']),
-        ...mapGetters(['avatarLink']),
-        avatar() {
-            return this.isNew
-                ? this.avatarLink
-                : route('core.avatars.show', this.comment.owner.avatarId || 'null', false).toString();
+        ...mapState(['user']),
+        isEmpty() {
+            return this.count === 0;
         },
-        highlightTaggedUsers() {
-            let { body } = this.comment;
-
-            this.comment.taggedUserList.forEach((user) => {
-                const highlighted = `${'<span class="highlight">@'}${user.fullName}</span>`;
-                body = body.replace(`@${user.fullName}`, highlighted);
-            });
-
-            return body;
-        },
-        isEditing() {
-            return this.originalBody !== null;
+        filteredComments() {
+            return this.query
+                ? this.comments.filter(comment => comment.body.toLowerCase()
+                    .indexOf(this.query.toLowerCase()) > -1
+                    || comment.owner.fullName.toLowerCase().indexOf(this.query.toLowerCase()) > -1)
+                : this.comments;
         },
     },
 
     data() {
         return {
-            controls: false,
-            dialog: false,
-            originalBody: null,
+            comments: [],
+            count: 0,
+            offset: 0,
+            comment: null,
+            loading: false,
+            query: null,
+            path: this.$route.path,
         };
     },
 
+    created() {
+        this.get();
+    },
+
     methods: {
-        cancelAdd() {
-            this.comment.body = this.originalBody;
-            this.controls = false;
-            this.originalBody = null;
-            this.$emit('cancel-edit');
+        get() {
+            this.loading = true;
+
+            axios.get(route('core.comments.index', [], false), { params: this.getParams() }).then(({ data }) => {
+                this.comments = this.offset
+                    ? this.comments.concat(data.comments)
+                    : data.comments;
+
+                this.count = data.count;
+                this.offset = this.comments.length;
+                this.loading = false;
+            }).catch((error) => {
+                this.loading = false;
+                this.handleError(error);
+            });
         },
-        update() {
+        getParams() {
+            return {
+                id: this.id,
+                type: this.type,
+                offset: this.offset,
+                paginate: this.paginate,
+            };
+        },
+        refresh() {
+            this.offset = 0;
+            this.get();
+        },
+        emptyComment() {
+            return {
+                body: '',
+                taggedUserList: [],
+                owner: {
+                    avatarId: this.user.avatarId,
+                    fullName: this.user.fullName,
+                },
+            };
+        },
+        create() {
+            if (this.comment) {
+                return;
+            }
+
+            this.comment = this.emptyComment();
+
+            if (this.$refs.card.collapsed) {
+                this.$refs.card.toggle();
+            }
+        },
+        add() {
             if (!this.comment.body.trim()) {
                 return;
             }
 
-            this.controls = false;
+            this.loading = true;
 
-            if (this.comment.body === this.originalBody) {
-                this.originalBody = null;
-                return;
-            }
+            axios.post(route('core.comments.store', [], false), this.postParams()).then(({ data }) => {
+                this.comments.unshift(data.comment);
+                this.count = data.count;
+                this.offset++;
+                this.comment = null;
+                this.loading = false;
+            }).catch((error) => {
+                this.loading = false;
+                this.handleError(error);
+            });
+        },
+        postParams() {
+            return {
+                id: this.id,
+                type: this.type,
+                body: this.comment.body,
+                taggedUserList: this.comment.taggedUserList,
+                path: this.path,
+            };
+        },
+        update(comment) {
+            this.syncTaggedUsers(comment);
+            comment.path = this.path;
+            this.loading = true;
 
-            this.$emit('save-comment');
+            axios.patch(route('core.comments.update', comment.id, false), comment).then(({ data }) => {
+                Object.assign(comment, data.comment);
+                this.loading = false;
+            }).catch((error) => {
+                this.loading = false;
+                this.handleError(error);
+            });
+        },
+        syncTaggedUsers(comment) {
+            comment.taggedUserList.forEach((user, index) => {
+                if (!comment.body.includes(user.fullName)) {
+                    comment.taggedUserList.splice(index, 1);
+                }
+            });
+        },
+        destroy(index) {
+            this.loading = true;
 
-            this.originalBody = null;
+            axios.delete(route('core.comments.destroy', this.comments[index].id, false)).then(({ data }) => {
+                this.comments.splice(index, 1);
+                this.count = data.count;
+                this.loading = false;
+            }).catch((error) => {
+                this.$parent.$parent.loading = false;
+                this.handleError(error);
+            });
         },
     },
 };
 
 </script>
 
-<style>
+<style scoped>
 
-    img.avatar {
-        border: 1px solid orangered;
-    }
-
-    span.highlight {
-        color: #3097d1;
-    }
-
-    span.comment-body {
-        word-break: break-all;
+    .wrapper {
+        max-height: 500px;
+        overflow-y: auto;
     }
 
 </style>
