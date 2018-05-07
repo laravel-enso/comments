bda3f71f979568cd5c2f481c8059c014db277fb0
Mihai <mihai.ocneanu@gmail.com>
2018-05-07 11:49:53 +0000

    Apply fixes from StyleCI

--- src/app/Notifications/CommentTagNotification.php
+++ src/app/Notifications/CommentTagNotification.php
@@ -10,7 +10,6 @@ use Illuminate\Notifications\Messages\BroadcastMessage;
 
 class CommentTagNotification extends Notification implements ShouldQueue
 {
-
     use Queueable;
 
     private $commentable;
@@ -33,7 +32,7 @@ class CommentTagNotification extends Notification implements ShouldQueue
     {
         return new BroadcastMessage([
             'level' => 'info',
-            'body'  => __('You were just tagged') . ': ' . $this->body,
+            'body' => __('You were just tagged').': '.$this->body,
         ]);
     }
 
@@ -42,12 +41,12 @@ class CommentTagNotification extends Notification implements ShouldQueue
         return (new MailMessage())
             ->view('emails.tagged',
                 [
-                    'intro'       => __('You were tagged in a message posted in') . ': ' . config('app.name'),
+                    'intro' => __('You were tagged in a message posted in').': '.config('app.name'),
                     'messageBody' => $this->body,
-                    'action'      => __('To answer, click the button below.'),
-                    'ending'      => __('Thank you'),
-                    'appName'     => config('app.name'),
-                    'appURL'      => config('app.url') . $this->path,
+                    'action' => __('To answer, click the button below.'),
+                    'ending' => __('Thank you'),
+                    'appName' => config('app.name'),
+                    'appURL' => config('app.url').$this->path,
                 ]);
     }
 
