67c9ce068f61560fab6095f9ba5c845eefb13ca1
mishu <mishu@xtelecom.ro>
2018-05-28 13:21:00 +0300

    updates for the new structure manager

--- src/database/migrations/2017_01_01_144000_create_structure_for_comments_manager.php
+++ src/database/migrations/2017_01_01_144000_create_structure_for_comments_manager.php
@@ -9,10 +9,10 @@ class CreateStructureForCommentsManager extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'core.comments.index', 'description' => 'List comments for commentable', 'type' => 0, 'default' => true],
-        ['name' => 'core.comments.store', 'description' => 'Create comment', 'type' => 1, 'default' => true],
-        ['name' => 'core.comments.update', 'description' => 'Update edited comment', 'type' => 1, 'default' => true],
-        ['name' => 'core.comments.destroy', 'description' => 'Delete comment', 'type' => 1, 'default' => true],
-        ['name' => 'core.comments.getTaggableUsers', 'description' => 'Get taggable users list', 'type' => 0, 'default' => true],
+        ['name' => 'core.comments.index', 'description' => 'List comments for commentable', 'type' => 0, 'is_default' => true],
+        ['name' => 'core.comments.store', 'description' => 'Create comment', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.comments.update', 'description' => 'Update edited comment', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.comments.destroy', 'description' => 'Delete comment', 'type' => 1, 'is_default' => true],
+        ['name' => 'core.comments.getTaggableUsers', 'description' => 'Get taggable users list', 'type' => 0, 'is_default' => true],
     ];
 }
