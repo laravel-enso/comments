3cc45c6b2372aba40823e99bef5500576682e7b1
mishu <mishu@xtelecom.ro>
2018-07-05 19:03:28 +0300

    fixes ASP

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
-    {o
+    {
         $this->publishesAll();
         $this->loadDependencies();
     }
