a825653137ab770ede6856092f689cf0187e0b0e
mishu <mishu@xtelecom.ro>
2018-08-24 12:04:05 +0300

    implements enso/helper's config reader

--- src/app/Classes/ConfigMapper.php
+++ src/app/Classes/ConfigMapper.php
@@ -2,28 +2,10 @@
 
 namespace LaravelEnso\CommentsManager\app\Classes;
 
-use LaravelEnso\CommentsManager\app\Exceptions\CommentConfigException;
+use LaravelEnso\Helpers\app\Classes\MorphableConfigMapper;
 
-class ConfigMapper
+class ConfigMapper extends MorphableConfigMapper
 {
-    private $type;
-
-    public function __construct(string $type)
-    {
-        $this->type = $type;
-    }
-
-    public function class()
-    {
-        $commentable = config('enso.comments.commentables.'.$this->type);
-
-        if (is_null($commentable)) {
-            throw new CommentConfigException(__(
-                'Entity ":entity" does not exist in enso/comments.php config file',
-                ['entity' => $this->type]
-            ));
-        }
-
-        return $commentable;
-    }
+    protected $configPrefix = 'enso.comments';
+    protected $morphableKey = 'commentables';
 }
--- src/app/Exceptions/CommentConfigException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace LaravelEnso\CommentsManager\app\Exceptions;
-
-use LaravelEnso\Helpers\app\Exceptions\EnsoException;
-
-class CommentConfigException extends EnsoException
-{
-}
