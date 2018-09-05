4ecf14dfbcec76d5ae7a7e1dd0e0788230150f8f
mishu <mishu@xtelecom.ro>
2018-09-05 11:06:36 +0300

    updates files structure to match L5.7's flat resources folder

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -31,11 +31,11 @@ class AppServiceProvider extends ServiceProvider
         ], 'enso-config');
 
         $this->publishes([
-            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+            __DIR__.'/resources/js' => resource_path('js'),
         ], 'comments-assets');
 
         $this->publishes([
-            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+            __DIR__.'/resources/js' => resource_path('js'),
         ], 'enso-assets');
 
         $this->publishes([
