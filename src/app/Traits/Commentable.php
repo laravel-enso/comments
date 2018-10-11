e0ab438a9b4d2390b7fdb611b2d6664ee88f921f
mishu <mishu@xtelecom.ro>
2018-10-11 08:26:59 +0300

    adds `onDelete` config option / improves `Commentable` trait

--- src/app/Traits/Commentable.php
+++ src/app/Traits/Commentable.php
@@ -3,9 +3,28 @@
 namespace LaravelEnso\CommentsManager\app\Traits;
 
 use LaravelEnso\CommentsManager\app\Models\Comment;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 trait Commentable
 {
+    public static function bootCommentable()
+    {
+        self::deleting(function ($model) {
+            if (config('enso.comments.onDelete') === 'restrict'
+                && $model->comments()->first() !== null) {
+                throw new ConflictHttpException(
+                    __('The entity has comments and cannot be deleted')
+                );
+            }
+        });
+
+        self::deleted(function ($model) {
+            if (config('enso.comments.onDelete') === 'cascade') {
+                $model->comments()->delete();
+            }
+        });
+    }
+
     public function comments()
     {
         return $this->morphMany(Comment::class, 'commentable');
