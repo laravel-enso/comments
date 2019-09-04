d18a961b755f8416276bd6e1bcc4f52911f363ab
Mihai <mihai.ocneanu@gmail.com>
2019-09-04 14:54:34 +0300

    removed superfluous validation

--- src/app/Http/Requests/ValidateCommentFetch.php
+++ src/app/Http/Requests/ValidateCommentFetch.php
@@ -3,14 +3,11 @@
 namespace LaravelEnso\Comments\app\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use LaravelEnso\Comments\app\Exceptions\CommentException;
 
 class ValidateCommentFetch extends FormRequest
 {
     public function authorize()
     {
-        $this->checkParams();
-
         return true;
     }
 
@@ -20,14 +17,5 @@ class ValidateCommentFetch extends FormRequest
             'commentable_id' => 'required',
             'commentable_type' => 'required|string',
         ];
-    }
-
-    private function checkParams()
-    {
-        if (! class_exists($this->get('commentable_type'))) {
-            throw new CommentException(
-                'The "commentable_type" property must be a valid model class'
-            );
-        }
     }
 }
