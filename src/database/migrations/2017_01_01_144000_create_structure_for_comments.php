0a381e3cff2422b3f53ea5d4cbf8ee30a4ec1501
mishu <mishu@xtelecom.ro>
2020-03-14 18:06:48 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_144000_create_structure_for_comments.php
+++ src/database/migrations/2017_01_01_144000_create_structure_for_comments.php
@@ -1,14 +1,13 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForComments extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.comments.index', 'description' => 'List comments for commentable', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.comments.store', 'description' => 'Create comment', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.comments.update', 'description' => 'Update edited comment', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.comments.destroy', 'description' => 'Delete comment', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.comments.index', 'description' => 'List comments for commentable', 'is_default' => true],
+        ['name' => 'core.comments.store', 'description' => 'Create comment', 'is_default' => true],
+        ['name' => 'core.comments.update', 'description' => 'Update edited comment', 'is_default' => true],
+        ['name' => 'core.comments.destroy', 'description' => 'Delete comment', 'is_default' => true],
     ];
 }
