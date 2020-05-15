f5531883c15f9246e01676292bc882f34ed96a9d
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2020-05-15 18:43:30 +0300

    adds controller for customizing taggable users

---
+++ src/App/Http/Controllers/Users.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LaravelEnso\Comments\App\Http\Controllers;
+
+use LaravelEnso\Core\App\Http\Controllers\Administration\User\Options as CoreUserOptions;
+
+class Users extends CoreUserOptions
+{
+}
--- src/database/migrations/2017_01_01_144000_create_structure_for_comments.php
+++ src/database/migrations/2017_01_01_144000_create_structure_for_comments.php
@@ -5,6 +5,7 @@ use LaravelEnso\Migrator\App\Database\Migration;
 class CreateStructureForComments extends Migration
 {
     protected $permissions = [
+        ['name' => 'core.comments.users', 'description' => 'Get taggable users options for select', 'is_default' => true],
         ['name' => 'core.comments.index', 'description' => 'List comments for commentable', 'is_default' => true],
         ['name' => 'core.comments.store', 'description' => 'Create comment', 'is_default' => true],
         ['name' => 'core.comments.update', 'description' => 'Update edited comment', 'is_default' => true],
