93d47c21831ea607bd9271db52c18718070b5006
mishu <mishu@xtelecom.ro>
2020-06-25 08:06:10 +0300

    fixes store controller

--- src/Http/Controllers/Store.php
+++ src/Http/Controllers/Store.php
@@ -11,7 +11,7 @@ class Store extends Controller
 {
     public function __invoke(ValidateCommentStore $request, Comment $comment)
     {
-        $comment->fill($request->except('taggedUsers'));
+        $comment->fill($request->validatedExcept('taggedUsers'));
 
         tap($comment)->save()
             ->syncTags($request->get('taggedUsers'))
--- src/Http/Requests/ValidateCommentStore.php
+++ src/Http/Requests/ValidateCommentStore.php
@@ -2,8 +2,12 @@
 
 namespace LaravelEnso\Comments\Http\Requests;
 
+use LaravelEnso\Helpers\Traits\FiltersRequest;
+
 class ValidateCommentStore extends ValidateCommentFetch
 {
+    use FiltersRequest;
+
     public function rules()
     {
         return parent::rules() + [
