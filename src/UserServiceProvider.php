31d1d3bf9bb88ce5731fc067e7c8ff0e7e3e000e
reza <raftx24@gmail.com>
2020-06-26 11:46:18 +0430

    - fixes namespaces

--- src/UserServiceProvider.php
+++ src/UserServiceProvider.php
@@ -5,7 +5,7 @@ namespace LaravelEnso\Comments;
 use Illuminate\Support\ServiceProvider;
 use LaravelEnso\Comments\DynamicsRelations\Comments;
 use LaravelEnso\Comments\DynamicsRelations\CommentTags;
-use LaravelEnso\Core\App\Models\User;
+use LaravelEnso\Core\Models\User;
 use LaravelEnso\DynamicMethods\Services\Methods;
 
 class UserServiceProvider extends ServiceProvider
