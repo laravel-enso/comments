375496c7a5bf9003c6ea527de87d455188eb915c
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 13:19:02 +0200

    updated migrations

--- database/migrations/2017_01_01_143000_create_comment_user_pivot_table.php
+++ database/migrations/2017_01_01_143000_create_comment_user_pivot_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentUserPivotTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -25,4 +25,4 @@ class CreateCommentUserPivotTable extends Migration
     {
         Schema::drop('comment_user');
     }
-}
+};
--- database/migrations/2017_01_01_144000_create_structure_for_comments.php
+++ database/migrations/2017_01_01_144000_create_structure_for_comments.php
@@ -2,7 +2,7 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForComments extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'core.comments.users', 'description' => 'Get taggable users options for select', 'is_default' => true],
@@ -11,4 +11,4 @@ class CreateStructureForComments extends Migration
         ['name' => 'core.comments.update', 'description' => 'Update edited comment', 'is_default' => true],
         ['name' => 'core.comments.destroy', 'description' => 'Delete comment', 'is_default' => true],
     ];
-}
+};
